96a7921a1dd7e48b48897cf63e739c8c1b049135
PoornimaKarma <161825445+PoornimaKarma@users.noreply.github.com>
2024-04-14 21:54:50 +0530

    Update removeduplicacy.java

--- removeduplicatet.java
+++ removeduplicatet.java
@@ -2,7 +2,7 @@ package basic_program;
 
 import java.util.HashSet;
 
-public class removeduplicatet {
+public class removeduplicacy {
 
 	public static void main(String[] args) {
 		int ar[]= {3,4,4,5,2,5,7,8};
@@ -13,7 +13,7 @@ public class removeduplicatet {
 			}
 			for(int n : hs)
 			{
-				System.out.print(n+" ,");
+				System.out.print(n+" ");
 			}
 	}
 
